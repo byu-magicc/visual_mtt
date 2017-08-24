50756e6331bbb6d8f6eba9bd7c8dc9a195bc642a
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-08-23 18:09:30 -0600

    camera_sim reads from video file

--- src/camera_sim/camera_sim.cpp
+++ src/camera_sim/camera_sim.cpp
@@ -29,6 +29,8 @@ namespace camera_sim {
     // subscription
     void callback(const sensor_msgs::ImageConstPtr& data);
 
+    void play_video();
+
   private:
     // ROS
     ros::NodeHandle nh_;
@@ -38,7 +40,9 @@ namespace camera_sim {
     // image data
     cv::Mat frame_;
 
-    bool guessed_;
+    bool parameters_guessed_;
+    bool video_file_only_;
+    std::string video_path_;
 
     // camera manager class
     std::shared_ptr<camera_info_manager::CameraInfoManager> camera_manager_;
@@ -55,11 +59,21 @@ CameraSim::CameraSim()
   std::string camera_name, camera_info_url;
   nh_private.param<std::string>("camera_name",     camera_name,     "");
   nh_private.param<std::string>("camera_info_url", camera_info_url, "");
+  nh_private.param<std::string>("video_path",      video_path_,     "");
+
+  // TODO: remove
+  std::cout << camera_name << std::endl;
+  std::cout << camera_info_url << std::endl;
+  std::cout << video_path_ << std::endl;
+
+  video_file_only_ = false;
+  if (video_path_ != "")
+    video_file_only_ = true;
 
   // if the default camera_sim is being used, parameters are being guessed
   if (camera_name=="camera_sim")
   {
-    guessed_ = true;
+    parameters_guessed_ = true;
     ROS_WARN("camera sim: intrinsic camera parameters will be guessed");
   }
 
@@ -71,6 +85,11 @@ CameraSim::CameraSim()
   image_transport::ImageTransport it(nh_private); // use private node handle
   sub_ = nh_.subscribe("input", 1, &CameraSim::callback,  this);
   pub_ = it.advertiseCamera("image_raw", 1);
+
+  // if the video source is a rosbag, subscription is needed
+  // if the video source is a standard file, there is no need to subscribe
+  if (video_file_only_)
+    sub_.shutdown();
 }
 
 // ----------------------------------------------------------------------------
@@ -81,9 +100,6 @@ void CameraSim::callback(const sensor_msgs::ImageConstPtr& data)
   // when it's read from .mp4 file, eventually replacing the python script
   frame_ = cv_bridge::toCvCopy(data, "bgr8")->image;
 
-  // sanity check: compare dimensions of frame to .yaml (on first frame?)
-  // stuff, warning
-
   // convert to image transport
   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame_).toImageMsg();
   msg->header = data->header;
@@ -93,7 +109,7 @@ void CameraSim::callback(const sensor_msgs::ImageConstPtr& data)
   ci->header.stamp = data->header.stamp;
 
   // update the height, width, and principal point if parameters are guessed
-  if (guessed_)
+  if (parameters_guessed_)
   {
     ci->height = frame_.rows;
     ci->width = frame_.cols;
@@ -113,6 +129,74 @@ void CameraSim::callback(const sensor_msgs::ImageConstPtr& data)
   pub_.publish(msg, ci);
 }
 
+// ----------------------------------------------------------------------------
+
+void CameraSim::play_video()
+{
+  // only run this video loop if the video source is a standard file
+  if (!video_file_only_)
+    return;
+
+  cv::VideoCapture source(video_path_);
+
+  if (!source.isOpened())
+    ROS_ERROR("Invalid video file path");
+
+  while (ros::ok()) {
+
+    std::cout << "---" << std::endl;
+    auto tic = ros::Time::now();
+
+
+
+    // get the next frame; if the end of the video is reached, loop
+    source >> frame_;
+    if(frame_.empty())
+    {
+      ROS_INFO("Looping video");
+      source.release();
+      source.open(video_path_);
+      source >> frame_;
+    }
+
+    std::cout << "from file time: " << (ros::Time::now() - tic).toSec() << std::endl;
+    tic = ros::Time::now();
+
+    // convert frame to image transport message
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame_).toImageMsg();
+    msg->header.stamp = ros::Time::now();
+
+    std::cout << "convert time:   " << (ros::Time::now() - tic).toSec() << std::endl;
+    tic = ros::Time::now();
+
+    // get the camera info
+    sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(camera_manager_->getCameraInfo()));
+    ci->header.stamp = msg->header.stamp; // TODO: move stamps to pre-publish
+
+    std::cout << "info time:      " << (ros::Time::now() - tic).toSec() << std::endl;
+    tic = ros::Time::now();
+
+    // update the height, width, and principal point if parameters are guessed
+    // if (parameters_guessed_)
+    // {
+    //   ci->height = frame_.rows;
+    //   ci->width = frame_.cols;
+    //   // ci->K[0] = 700;                // fx (keep original guess)
+    //   // ci->K[4] = 700;                // fy (keep original guess)
+    //   ci->K[2] = (double)frame_.cols/2; // cx
+    //   ci->K[5] = (double)frame_.rows/2; // cy
+    // }       // TODO: generate fx, fy based on guessed FOV
+
+    // publish frame and camera info together (can only publish <msg>Ptr types)
+    // std::cout << "publishing" << std::endl;
+    pub_.publish(msg, ci);
+
+    std::cout << "publish time:   " << (ros::Time::now() - tic).toSec() << std::endl;
+
+  }
+}
+
+
 }
 
 // ----------------------------------------------------------------------------
@@ -124,6 +208,9 @@ int main(int argc, char **argv)
 
   // instantiate the CameraSim class
   camera_sim::CameraSim camera_sim;
+
+  // read video file if applicable
+  camera_sim.play_video();
 
   ros::spin();
   return 0;
