20cdd8a9ad77ea0e50b973b589349476c8ebd9e1
Mark Petersen <pmarke@byu.edu>
2021-02-18 17:20:50 -0700

    continued working on the homography projection

--- matlab/test.m
+++ matlab/test.m
@@ -14,7 +14,8 @@ norm(logm(R1'*R))
 norm(logm(R2'*R))
 
 %%
-rng('shuffle')
+% rng('shuffle')
+rng('default')
 w = diag([0.1,0.1,2])*rand(3,1);
 W = [0 -w(3) w(1); w(3) 0 -w(2); -w(1) w(2) 0];
 R = expm(W);
@@ -45,14 +46,21 @@ U2 = [v2,u2,cross(v2,u2)];
 W1 = [HL*v2, HL*u1, cross(HL*v2,HL*u1)];
 W2 = [HL*v2, HL*u2, cross(HL*v2,HL*u2)];
 % N
-% N1 = ssm(v2)*u1
-% N2 = ssm(v2)*u2
-R
-R1 = W1*U1'
-R2 = W2*U2'
+N1 = cross(v2,u1);
+N2 = cross(v2,u2);
+R;
+R1 = W1*U1';
+R2 = W2*U2';
 % Tn = normalize(T)
-% T1 = (HL-R1)*N1
-% T2 = (HL-R2)*N2
+T1 = (HL-R1)*N1;
+T2 = (HL-R2)*N2;
+
+H1 = R1 +T1*N1';
+H2 = R2 + T2*N2';
+
+Hp1 = project(R1,T1,N1)
+Hp2 = project(R2,T2,N2)
+Hp = project(R,T,N)
 
 
 function S = ssm(s)
@@ -65,4 +73,24 @@ S = [0 -z x; z 0 -y; -x y 0];
 
 end
 
+function H = project(R,T,N)
+
+if (N(3) <0)
+    T = T*(-1);
+    
+end
+% T = T/norm(T);
+V = logm(R);
+V = logm(R);
+w = [0;0;V(1,2)];
+W = [0 -w(3) w(1); w(3) 0 -w(2); -w(1) w(2) 0];
+R = expm(W);
+N = [0;0;1];
+H = R+T*N';
+% H(:,3) = H(:,3);
+H = H/norm(H);
+D = eig(H'*H);
+H = H/sqrt(D(2));
+
+end
 
